59848594a08a0eb31b14ca993cb46cb3284694bb
GustavoRocha <gustavorocha.iel@fieg.com.br>
2023-09-29 16:52:40 -0300

    Mudar a cor do fundo de acordo com cada tipo

--- Pokedex/assets/js/poke-api.js
+++ Pokedex/assets/js/poke-api.js
@@ -1,35 +1,33 @@
 const pokeApi = {}
 
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
+    const types = pokeDetail.types.map(typeSlot => typeSlot.type.name)
+    const [type] = types
 
-/*fetch nos retorna um promisse. Quanto vamos fazer uma requisição HTTP, a resposta 
-demora um determinado tempo
-Promisse é uma promessa de um resultado
+    pokemon.types = types
+    pokemon.type = type
+    pokemon.imagePath = pokeDetail.sprites.other.dream_world.front_default
 
-Um processamento assíncrono são processos realizados simultaneamente 
-sem que um necessite que outro termine para ser executado, o que é útil 
-para lidar com operações demoradas, como solicitações HTTP.
-*/
+    return pokemon
+}
+
+pokeApi.getPokemonDetail = pokemon => {
+    return fetch(pokemon.url)
+        .then(response => response.json())
+        .then(convertPokeApiDetailToPokemon)
+}
 
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
     return fetch(url)
         .then(response => response.json())
         .then(jsonBody => jsonBody.results)
-        .catch(error => console.log(error))
-
+        .then(pokemons => pokemons.map(pokeApi.getPokemonDetail))
+        .then(detailRequests => Promise.all(detailRequests))
+        .then(pokemonsDetails => pokemonsDetails)
 }
-
-/* O objeto 'response' é a resposta da solicitação HTTP e contém informações
-   como status, cabeçalhos e corpo da resposta. */
-
-/*O fetch é iniciado, e a solicitação HTTP é feita de forma assíncrona.
-O código continua a ser executado.
-
-Se a solicitação for bem-sucedida, o .then é executado, e a resposta é impressa no console.
-
-Se ocorrer um erro na solicitação, o .catch é executado, e o erro é impresso no console.
-
-Independentemente do resultado da solicitação, o .finally é executado, e a mensagem 
-"Requisição concluída" é impressa no console.*/
-
 
